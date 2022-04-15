cc0e127a7c185dd863066c342bb8b9941573cc44
guthemberg_interaminense_ext <guthemberg_interaminense_ext@carrefour.com>
2022-04-14 22:32:25 -0300

    injetando dependencia JPA para Entity Insumo

--- src/java/br/recife/edu/ifpe/model/negocio/Insumo.java
+++ src/java/br/recife/edu/ifpe/model/negocio/Insumo.java
@@ -5,12 +5,21 @@
  */
 package br.recife.edu.ifpe.model.negocio;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  *
  * @author ALUNO
  */
+@Entity
 public class Insumo {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private int id;
     private String nome;
     private String descricao;
